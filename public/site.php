2033b9b5dec4193971a50526da7128360b623256
Asper Hsu <asperwon@gmail.com>
2016-04-19 23:07:27 +0800

    merge active and deactive to single file

--- public/site.php
+++ public/site.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>AirMap Site List</title>
-
+		<link rel='shortcut icon' type='image/x-icon' href='favicon.png' />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.10.1/bootstrap-table.min.css">
 		<!--[if lt IE 9]>
@@ -17,51 +17,122 @@
 		</style>
 	</head>
 	<body>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a href="http://beta.hackfoldr.org/g0vairmap/g0v--riRTvbB0E5x" class=""><img src="image/g0v.png" class="img-responsive" alt="Image" style="max-height:40px; margin:5px 0;"></a>
+				</div>
+		
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav" role="tablist">
+						<li role="presentation" class="active">
+							<a href="#active" aria-controls="home" role="tab" data-toggle="tab" title="最後更新時間在一小時內">
+								<span class="glyphicon glyphicon-ok-sign"></span>
+								有效站點
+							</a>
+						</li>
+						<li role="presentation">
+							<a href="#deactive" aria-controls="tab" role="tab" data-toggle="tab" title="最後更新時間超過一小時">
+								<span class="glyphicon glyphicon-question-sign"></span>
+								過期站點
+							</a>
+						</li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<a href='/' class="btn btn-success navbar-btn">Map</a>
+						<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">DataSource <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="http://g0vairmap.3203.info/Data/ProbeCube_last.json">ProbeCube</a></li>
+								<li><a href="http://g0vairmap.3203.info/Data/EPA_last.json">EPA</a></li>
+								<li><a href="http://g0vairmap.3203.info/Data/LASS_last.json">LASS</a></li>
+								<li><a href="http://g0vairmap.3203.info/Data/Indie_last.json">Indie</a></li>
+								<li><a href="http://g0vairmap.3203.info/Data/Airbox_last.json">Airbox</a></li>
+								<li><a href="http://g0vairmap.3203.info/Data/webduino_last.json">Webduino</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>
+
 		<div class="container">
-			<h1>AirMap Site List
-				<a href='/' class="btn btn-success">Map</a>
-				<a href='/site' class="btn btn-primary">Site List</a>
-				<a href='/expire' class="btn btn-warning">Expired Site</a>
-			</h1>
-			<div class="row">
-				<div id="toolbar" class="">
-					<div class="form-inline" role="form">
-						<div class="form-group">
-							<span>Site Group</span>
-							<select name="siteGroup" class="form-control"></select>
-						</div>
+
+			<div class="toolbar site-active">
+				<div class="form-inline" role="form">
+					<div class="form-group">
+						<label>Site Group</label>
+						<select name="siteGroup" class="form-control"></select>
 					</div>
 				</div>
+			</div>
 
-				<table id="table" class="table table-striped" 
-					   data-toolbar="#toolbar"
-					   data-search="true" 
-					   data-toggle="table" data-detail-view="true" 
-					   data-detail-formatter="bsTable.formatter.detail" 
-					   >
-					<thead>
-						<tr>
-							<th data-formatter="bsTable.formatter.sn">#</th>
-							<th data-field="SiteGroup" data-sortable="true">Site Group</th>
-							<th data-field="SiteName" data-sortable="true" data-searchable="true" data-formatter="bsTable.formatter.siteName">Site Name</th>
-							<th data-field="Maker" data-sortable="true" data-searchable="true">Maker</th>
-							<th data-field="LatLng" data-sortable="true" data-formatter="bsTable.formatter.location">Location</th>
-							<th data-field="Data.Create_at" data-sortable="true" data-formatter="bsTable.formatter.updateTime">Update Time</th>
-						</tr>
-					</thead>
-				</table>
+			<div class="toolbar site-deactive">
+				<div class="form-inline" role="form">
+					<div class="form-group">
+						<label>Site Group</label>
+						<select name="siteGroup" class="form-control"></select>
+					</div>
+				</div>
+			</div>
+
+			<div role="tabpanel">
+				<div class="tab-content">
+					<div role="tabpanel" class="tab-pane active" id="active">
+						<table id="table-active" class="bsTable table table-striped" 
+							   data-url="json/airmap.json"
+							   data-toolbar=".toolbar.site-active"
+							   data-search="true" 
+							   data-toggle="table" data-detail-view="true" 
+							   data-detail-formatter="bsTable.formatter.detail" 
+							   >
+							<thead>
+								<tr>
+									<th data-formatter="bsTable.formatter.sn">#</th>
+									<th data-field="SiteGroup" data-sortable="true">Site Group</th>
+									<th data-field="SiteName" data-sortable="true" data-searchable="true" data-formatter="bsTable.formatter.siteName">Site Name</th>
+									<th data-field="Maker" data-sortable="true" data-searchable="true">Maker</th>
+									<th data-field="LatLng" data-sortable="true" data-formatter="bsTable.formatter.location">Location</th>
+									<th data-field="Data.Create_at" data-sortable="true" data-formatter="bsTable.formatter.updateTime">Update Time</th>
+								</tr>
+							</thead>
+						</table>
+					</div>
+					<div role="tabpanel" class="tab-pane" id="deactive">
+						<table id="table-deactive" class="bsTable table table-striped" 
+							   data-url="json/deactivesite.json"
+							   data-toolbar=".toolbar.site-deactive"
+							   data-search="true" 
+							   data-toggle="table" data-detail-view="true" 
+							   data-detail-formatter="bsTable.formatter.detail" 
+							   >
+							<thead>
+								<tr>
+									<th data-formatter="bsTable.formatter.sn">#</th>
+									<th data-field="SiteGroup" data-sortable="true">Site Group</th>
+									<th data-field="SiteName" data-sortable="true" data-searchable="true" data-formatter="bsTable.formatter.siteName">Site Name</th>
+									<th data-field="Maker" data-sortable="true" data-searchable="true">Maker</th>
+									<th data-field="LatLng" data-sortable="true" data-formatter="bsTable.formatter.location">Location</th>
+									<th data-field="Data.Create_at" data-sortable="true" data-formatter="bsTable.formatter.updateTime">Update Time</th>
+								</tr>
+							</thead>
+						</table>
+					</div>
+				</div>
 			</div>
 
 			<hr>
 
-			<div id="footer" class="well well-sm">
-				DataSource: http://g0vairmap.3203.info/Data/
-				<code>ProbeCube_last.json</code>,
-				<code>EPA_last.json</code>,
-				<code>LASS_last.json</code>,
-				<code>Indie_last.json</code>,
-				<code>Airbox_last.json</code>,
-				<code>webduino_last.json</code>
+			<div id="footer" class="well well-sm" style="text-align: center;">
+				Asper &copy; 2016 <a href='https://github.com/Aspertw/AirMap'>GitHub</a>
 			</div>
 		</div>
 		
@@ -89,13 +160,10 @@
 				siteName = decodeURIComponent(siteName);
 				bsTable.event.filter('SiteName', siteName);
 			}
-
-			var action = location.pathname.substr(1);
-			var dataSource = {
-				site: 'json/airmap.json',
-				expire: 'json/outofdate.json',
-			}
-			bsTable.event.loadData(dataSource[action]);
+			
+			$(".bsTable").on('load-success.bs.table', function(e, data){
+				bsTable.event.loadSiteGroup($(this), data);
+			});
 
 		});
 
@@ -125,6 +193,10 @@
 					return human + ' (' + dataTime + ')';
 				},
 				siteName: function(name){
+					return name;
+
+					// TODO: site hash filter
+
 					var url = window.location.hash + "#" + name;
 					return "<a href='" + url + "' target='_blank'>" + name + "</a>";
 				}
@@ -174,9 +246,10 @@
 				}
 			},
 			event: {
-				loadSiteGroup: function(data){
-					var $select = $("select[name=siteGroup");
+				loadSiteGroup: function($table, data){
+					var $select = $( $table.data('toolbar') ).find('select[name=siteGroup]');
 					$select.html(bsTable.generate.siteOptions(data));
+					
 					$select.change(function(){
 						var group = $(this).val();
 
@@ -185,23 +258,11 @@
 							filterValue = group;
 						}
 
-						bsTable.event.filter('SiteGroup', filterValue);
+						bsTable.event.filter($table, 'SiteGroup', filterValue);
 					});
 				},
-				loadData: function(url){
-					if(!url){ return false; }
-
-					$table = $("#table");
-
-					$table.bootstrapTable('showLoading');
-					$.getJSON(url, function(data){
-						$table.bootstrapTable('load', data);
-						bsTable.event.loadSiteGroup(data);
-						$table.bootstrapTable('hideLoading');
-					});
-				},
-				filter: function(key, value){
-					var filter = $("#table").data('filter') || {};
+				filter: function($table, key, value){
+					var filter = $table.data('filter') || {};
 
 					if( value == null && filter[key] ){
 						delete filter[key];
@@ -209,8 +270,8 @@
 						filter[key] = value;
 					}					
 
-					$("#table").data('filter', filter)
-							   .bootstrapTable('filterBy', filter);
+					$table.data('filter', filter)
+						  .bootstrapTable('filterBy', filter);
 				},
 			}
 		}
